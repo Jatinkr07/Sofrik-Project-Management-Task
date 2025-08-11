f8d3be46372c02a8fdcc040e07695b53cbb4ab26
Jatinkr07 <jatinkumar07911@gmail.com>
2025-08-11 12:28:57 +0530

    fixed done

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,12 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: [process.env.CLIENT_URL || "http://localhost:5173"],
+    credentials: true,
+  })
+);
 
 connectDB();
 
